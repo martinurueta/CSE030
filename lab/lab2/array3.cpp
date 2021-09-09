b3c85d689bdefc5fa8967a9b8ad30d312b989dad
Martin <uruetaiiimartin@gmail.com>
2021-09-09 11:41:39 -0700

    Lab 2 semi complete

--- lab/lab2/array3.cpp
+++ lab/lab2/array3.cpp
@@ -4,5 +4,37 @@
 using namespace std;
 
 int main(){
+    int arraySize;
+    int negCount = 0;
+    string arrayRow;
+    cout << "Enter the size of a 2D array:";
+    cin >> arraySize;
+    if (arraySize > 10 || arraySize < 1){
+        if(arraySize > 10){
+            cout << "ERROR: your array is too large! Enter 1 to 10";
+            return 0;
+        }else{
+            cout << "ERROR: your array is too short! Enter 1 to 10";
+            return 0;
+        }
+        
+    }
+    cin.ignore();
+    int array[arraySize][arraySize];
+     for (int i = 1; i <= arraySize; i++){
+     cout << "Enter the values in the array for row " << i << " seperated by a space, and press enter:";
+        for (int j = 0; j < arraySize; j++)
+        {
+        scanf ("%d",&array[i][j]);
+        if (array[i][j] < 0){
+            negCount++;
+        }
+    }
+}
+    if (negCount > 0){
+        cout << "There are "<< negCount << "negative values!";
+    }else{
+        cout << "All values are non-negative!";
+    }
     return 0;
 }
---
+++ lab/lab2/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if (values[i] != 32){
+            array[j] = array[j] * 10 + (values[i]-48);// convert string to integer in the array
+            j++;
+        }
